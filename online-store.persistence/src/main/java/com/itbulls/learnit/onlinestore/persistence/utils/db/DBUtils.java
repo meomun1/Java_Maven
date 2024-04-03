2c2555693588eb410d6b4086fd2c3107daa31125
meomun1 <maluong1504@gmail.com>
2024-04-03 23:59:10 +0700

    change in persistence

--- online-store.persistence/src/main/java/com/itbulls/learnit/onlinestore/persistence/utils/db/DBUtils.java
+++ online-store.persistence/src/main/java/com/itbulls/learnit/onlinestore/persistence/utils/db/DBUtils.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBUtils {
 	
 	private static final String JDBC_MYSQL_HOST = "jdbc:mysql://localhost:3306/";
-	private static final String DB_NAME = "learn_it_db";
+	private static final String DB_NAME = "final_task";
 	private static final String USERNAME = "root";
 	private static final String PASSWORD = "MaL1504@";
 	
