34593eedecf1440f5991995f5671ea1b662827dd
meomun1 <maluong1504@gmail.com>
2024-05-11 19:36:39 +0700

    Remove unecessary files and make changes for new MVC model configuration

--- online-store.web/src/main/java/com/itbulls/learnit/onlinestore/servlets/exceptions/ErrorHandlerServlet.java
+++ online-store.web/src/main/java/com/itbulls/learnit/onlinestore/servlets/exceptions/ErrorHandlerServlet.java
@@ -13,23 +13,19 @@ import java.io.PrintWriter;
  */
 @WebServlet(urlPatterns = "/error-handler")
 public class ErrorHandlerServlet extends HttpServlet {
- 
+
    public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-      // Analyze the servlet exception       
-      Throwable throwable = (Throwable)
-      request.getAttribute("jakarta.servlet.error.exception");
-      Integer statusCode = (Integer)
-      request.getAttribute("jakarta.servlet.error.status_code");
-      String servletName = (String)
-      request.getAttribute("jakarta.servlet.error.servlet_name");
-         
+         throws ServletException, IOException {
+      // Analyze the servlet exception
+      Throwable throwable = (Throwable) request.getAttribute("jakarta.servlet.error.exception");
+      Integer statusCode = (Integer) request.getAttribute("jakarta.servlet.error.status_code");
+      String servletName = (String) request.getAttribute("jakarta.servlet.error.servlet_name");
+
       if (servletName == null) {
          servletName = "Unknown";
       }
-      String requestUri = (String)
-      request.getAttribute("jakarta.servlet.error.request_uri");
-      
+      String requestUri = (String) request.getAttribute("jakarta.servlet.error.request_uri");
+
       if (requestUri == null) {
          requestUri = "Unknown";
       }
@@ -39,41 +35,55 @@ public class ErrorHandlerServlet extends HttpServlet {
 
       PrintWriter out = response.getWriter();
       String title = "Error/Exception Information";
-      String docType =
-         "<!doctype html public \"-//w3c//dtd html 4.0 " +
-         "transitional//en\">\n";
-         
+      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " +
+            "transitional//en\">\n";
+
       out.println(docType +
-         "<html>\n" +
-         "<head><title>" + title + "</title></head>\n" +
-         "<body bgcolor = \"#f0f0f0\">\n");
+            "<html>\n" +
+            "<head><title>" + title + "</title></head>\n" +
+            "<body bgcolor = \"#f0f0f0\">\n");
 
       if (throwable == null && statusCode == null) {
-         out.println("<h2>Error information is missing</h2>");
-         out.println("Please return to the <a href=\"" + 
-            response.encodeURL("http://localhost:8080/" + getServletContext().getContextPath()) + 
-            "\">Home Page</a>.");
+         out.println("<style>");
+         out.println(
+               "body { display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh; }");
+         out.println("h2 { color: #333; font-size: 2em; }");
+         out.println("p, a { color: #007BFF; text-decoration: none; font-size: 1.5em; }");
+         out.println("p { margin-top: 10px; font-size: 1.2em; color: #555; }");
+         out.println("</style>");
+         out.println("<h2>Error information is missing 404</h2>"); // h2 tag
+         out.println("<p>Please return to the</p>"); // p tag
+         out.println("<a href=\"/home-page" +
+               response.encodeURL("http://localhost:8080/" + getServletContext().getContextPath()) +
+               "\">Home Page</a>.");
       }
-      
+
       if (statusCode != null) {
-         out.println("The status code : " + statusCode);
-         out.println("<h2>Error information</h2>");
-         out.println("Servlet Name : " + servletName + "</br></br>");
-         out.println("The request URI: " + requestUri + "<br><br>");
-         
+         out.println("<style>");
+         out.println(
+               "body { display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh; }");
+         out.println("h2 { color: #333; font-size: 2em; }");
+         out.println("p, a { color: #007BFF; text-decoration: none; font-size: 1.5em; }");
+         out.println("p { margin-top: 10px; font-size: 1.2em; color: #555; }");
+         out.println("</style>");
+         out.println("<h2>Error information is missing 404</h2>"); // h2 tag
+         out.println("<p>Please return to the</p>"); // p tag
+         out.println("<a href=\"/home-page" +
+               response.encodeURL("http://localhost:8080/" + getServletContext().getContextPath()) +
+               "\">Home Page</a>.");
       }
       if (throwable != null) {
-    	  out.println("Exception Type : " + throwable.getClass( ).getName( ) + "</br></br>");
-          out.println("The exception message: " + throwable.getMessage( ));
+         out.println("Exception Type : " + throwable.getClass().getName() + "</br></br>");
+         out.println("The exception message: " + throwable.getMessage());
       }
       out.println("</body>");
       out.println("</html>");
    }
-   
+
    // Method to handle POST method request.
    public void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-      
+         throws ServletException, IOException {
+
       doGet(request, response);
    }
 }
